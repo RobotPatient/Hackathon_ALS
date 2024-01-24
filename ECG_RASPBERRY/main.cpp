a7575544f6df462489244c60f94b062ed48afe7f
joris <joris@bolmail.nl>
2024-01-24 14:55:29 +0100

    Update main.cpp

--- ECG_RASPBERRY/main.cpp
+++ ECG_RASPBERRY/main.cpp
@@ -7,17 +7,18 @@
 char leadRed, leadBlack, leadWhite;
 
 int main() {
+    while(true) {
 
-    //create new instance
-    sensorHubI2C shi;
+        //create new instance
+        sensorHubI2C shi;
 
-    //start I2C connection
-    if (!shi.begin()) {
-        std::cerr << "Failed to initialize I2C";
-        return 1;
-    }
+        //start I2C connection
+        if (!shi.begin()) {
+            std::cerr << "Failed to initialize I2C";
+            return 1;
+        }
 
-    if (shi.readData(leadRed, leadBlack, leadWhite)) {
+        if (shi.readData(leadRed, leadBlack, leadWhite)) {
 //        if (leadRed == true) {
 //            std::cout << "The red lead is connected" << std::endl;
 //        } else std::cout << "The red lead is NOT connected" << std::endl;
@@ -29,12 +30,14 @@ int main() {
 //        if (leadRed == true) {
 //            std::cout << "The white lead is connected" << std::endl;
 //        } else std::cout << "The red lead is NOT connected" << std::endl;
-        std::cout << "char 1: " << leadRed << std::endl;
-        std::cout << "char 2: " << leadBlack << std::endl;
-        std::cout << "char 3: " << leadWhite << std::endl;
-    } else {
-        std::cerr << "failed to read data from the I2C bus" << std::endl;
-        return 1;
+            std::cout << "char 1: " << leadRed << std::endl;
+            std::cout << "char 2: " << leadBlack << std::endl;
+            std::cout << "char 3: " << leadWhite << std::endl;
+        } else {
+            std::cerr << "failed to read data from the I2C bus" << std::endl;
+            return 1;
+        }
+        Sleep(10000);
     }
 
     return 0;

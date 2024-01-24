e7dc104c9e930a399bf214229583f94fe43db2a9
joris <joris@bolmail.nl>
2024-01-24 16:58:43 +0100

    update

--- ECG_RASPBERRY/I2C/I2C.h
+++ ECG_RASPBERRY/I2C/I2C.h
@@ -10,7 +10,7 @@
 class sensorHubI2C
 {
 public:
-    sensorHubI2C(const char* i2cDevice = "/dev/i2c-1", int i2cAddr = 0x44);
+    sensorHubI2C(const char* i2cDevice = "/dev/i2c-1", int i2cAddr = 0x2b);
     ~sensorHubI2C();
 
     bool begin();

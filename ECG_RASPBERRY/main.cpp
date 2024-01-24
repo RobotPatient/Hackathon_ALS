423b003906df786d105fd1736fd045d88172ed2c
joris <joris@bolmail.nl>
2024-01-24 13:21:36 +0100

    Update main.cpp

--- ECG_RASPBERRY/main.cpp
+++ ECG_RASPBERRY/main.cpp
@@ -6,40 +6,32 @@
 
 bool leadRed, leadBlack, leadWhite;
 
-int main()
-{
-    try
-    {
+int main() {
 
-        //create new instance
-        sensorHubI2C shi;
+    //create new instance
+    sensorHubI2C shi;
 
-        //start I2C connection
-        if(!shi.begin())
-        {
-            std::cerr << "Failed to initialize I2C"
-            return 1;
-        }
-
-        if(shi.readData(leadRed, leadBlack, leadWhite))
-        {
-            if(leadRed == true) {
-                std::cout << "The red lead is connected" << std::endl;
-            } else std::cout << "The red lead is NOT connected" << std::endl;
-
-            if(leadRed == true) {
-                std::cout << "The black lead is connected" << std::endl;
-            } else std::cout << "The red lead is NOT connected" << std::endl;
-
-            if(leadRed == true) {
-                std::cout << "The white lead is connected" << std::endl;
-            } else std::cout << "The red lead is NOT connected" << std::endl;
-        }
-        else
-        {
-            std::cerr << "failed to read data from the I2C bus" << std::endl;
-            return  1;
-        }
-
+    //start I2C connection
+    if (!shi.begin()) {
+        std::cerr << "Failed to initialize I2C"
+        return 1;
     }
+
+    if (shi.readData(leadRed, leadBlack, leadWhite)) {
+        if (leadRed == true) {
+            std::cout << "The red lead is connected" << std::endl;
+        } else std::cout << "The red lead is NOT connected" << std::endl;
+
+        if (leadRed == true) {
+            std::cout << "The black lead is connected" << std::endl;
+        } else std::cout << "The red lead is NOT connected" << std::endl;
+
+        if (leadRed == true) {
+            std::cout << "The white lead is connected" << std::endl;
+        } else std::cout << "The red lead is NOT connected" << std::endl;
+    } else {
+        std::cerr << "failed to read data from the I2C bus" << std::endl;
+        return 1;
+    }
+    return 0;
 }

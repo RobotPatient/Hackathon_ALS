5152f9fd086f519ba883b905eda21bfbe985bf36
sudden <htin315@gmail.com>
2024-01-24 16:29:21 +0100

    Completed Firm ware for ECG chip with I2C

---
+++ ECG_CLION_EASE/src/I2C.h
@@ -0,0 +1,19 @@
+
+//
+// Created by Sudden on 1/24/2024.
+//
+
+#ifndef ECG_CLION_EASE_I2C_H
+#define ECG_CLION_EASE_I2C_H
+
+#define ECG_MODULE_ADDR 0x2A
+
+//String LLstr;
+//String ECGSTATUS;
+//String LAstr;
+//String RAstr;
+
+void initI2C();
+void requestEvent();
+
+#endif //ECG_CLION_EASE_I2C_H
--- ECG_CLION_EASE/src/main.cpp
+++ ECG_CLION_EASE/src/main.cpp
@@ -8,52 +8,28 @@
 
 */
 #include <Arduino.h>
-#include <Wire.h>
 #include "HeartBeat.h"
+#include "readInfrared.h"
+#include "I2C.h"
 
-#define ECG_MODULE_ADDR 0x2A
 #define HEARTBEAT_LEDPIN 14
 #define DEFAULT_HEARTBEAT_INTERVAL 1000
-
-// Sensor Definitions:
-
-#define SENSOR_LL A1  // PB08 red
-#define SENSOR_LA A2  // PB09 black
-#define SENSOR_RA A3  // PA04 white
 
 HeartBeat heartBeat = HeartBeat(); // (HEARTBEAT_LEDPIN, DEFAULT_HEARTBEAT_INTERVAL) ;
 
 void setup() {
+    Serial.begin(9600);
+    Serial.print("working");
     heartBeat.begin();
-    Wire.begin(ECG_MODULE_ADDR);     // join i2c bus with address #4
-    Wire.onReceive(receiveEvent);  // register event maybe for future usage
-    Wire.onRequest(requestEvent);    // register event
+    initI2C();
+    initECG();
 }
 
 void loop() {
     heartBeat.blink();
-}
-
-// function that executes whenever data is received from master
-// this function is registered as an event, see setup()
-// Maybe useful for future usage
-
-void receiveEvent(int howMany) {
-    (void)howMany;  // avoid compiler warning about unused parameter
-
-    while (1 < Wire.available())  // loop through all but the last
-    {
-        char c = Wire.read();  // receive byte as a character
-        Serial.print(c);       // print the character
-    }
-    int x = Wire.read();  // receive byte as an integer
-    Serial.println(x);    // print the integer
-}
-
-// function that executes whenever data is requested by master
-// this function is registered as an event, see setup()
-void requestEvent()
-{
-    Wire.write("012345"); // respond with message of 6 bytes (HLL, LLL, HLA, LLA, HRA, LRA)
-    // as expected by master
+    readValAndCheckConnection();
+    Serial.write(LLstatus);
+    Serial.write(RAstatus);
+    Serial.write(LAstatus);
+    delay(100);
 }

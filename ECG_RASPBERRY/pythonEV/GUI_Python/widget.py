e0d65abc17091d1afe90a02508c108693c3954df
joris <joris@bolmail.nl>
2024-01-25 11:46:37 +0100

    Update widget.py

--- ECG_RASPBERRY/pythonEV/GUI_Python/widget.py
+++ ECG_RASPBERRY/pythonEV/GUI_Python/widget.py
@@ -127,12 +127,12 @@ class InfoWidget(QWidget):
 
         #HB info
         self.heart_beat = QLabel("HB")
-        self.heart_beat.setStyleSheet("background-color: black; color: green; min-width: 200px; border-width: 2px; border-style: solid; border-color: gray; font-size: 40px; text-align: center")
+        self.heart_beat.setStyleSheet("background-color: black; color: green; min-width: 200px; border-width: 2px; border-style: solid; border-color: gray; font-size: 45px; text-align: center")
         self.layout().addWidget(self.heart_beat)
 
         #RP info
         self.resperation = QLabel("RP")
-        self.resperation.setStyleSheet("background-color: black; color: white; min-width: 200px; border-width: 2px; border-style: solid; border-color: gray; font-size: 40px; text-align: center")
+        self.resperation.setStyleSheet("background-color: black; color: white; min-width: 200px; border-width: 2px; border-style: solid; border-color: gray; font-size: 45px; text-align: center")
         self.layout().addWidget(self.resperation)
 
         #setup a timer
@@ -143,8 +143,8 @@ class InfoWidget(QWidget):
     def update_info(self):
         #update data fieldsS
         if state == 1:
-            self.heart_beat.setText(str(ecg_rate))
-            self.resperation.setText(str(rsp_rate))
+            self.heart_beat.setText(str(ecg_rate) + "BPM")
+            self.resperation.setText(str(rsp_rate) + "BPM")
         else:
             self.heart_beat.setText('0')
             self.resperation.setText('0')

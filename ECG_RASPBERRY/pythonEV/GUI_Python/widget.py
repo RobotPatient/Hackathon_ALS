7c9242e57d85bdadf8e2441a080872b51a063ef1
joris <joris@bolmail.nl>
2024-01-25 10:10:19 +0100

    Update widget.py

--- ECG_RASPBERRY/pythonEV/GUI_Python/widget.py
+++ ECG_RASPBERRY/pythonEV/GUI_Python/widget.py
@@ -96,7 +96,7 @@ class GraphWidget(QWidget):
             self.rsp15_part = np.roll(self.rsp15_part, -50)
 
 class info_widget(QWidget):
-    def __init__(selfe, parent=none):
+    def __init__(selfe, parent=None):
         super(InfoWidget, self).__init__(parent)
 
         #Ceates box for formatting
